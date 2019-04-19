9f62aef0f1ddae56bef8bbaefa68299773db4951
ges0909 <gerrit.schrader@gmail.com>
2019-04-19 10:44:04 +0200

    use of stream assertions

--- src/test/java/schrader/stream/test/StreamTest.java
+++ src/test/java/schrader/stream/test/StreamTest.java
@@ -12,219 +12,226 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.regex.Pattern;
 import java.util.stream.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Java6Assertions.fail;
 
+@DisplayName("Stream demo tests")
 class StreamTest {
 
     @Nested
-    @DisplayName("stream creation")
-    class StreamCreation {
+    class StreamCreationTest {
 
         @Test
-        void emptyStream() {
+        void empty() {
             final Stream<String> s = Stream.empty();
-            assertThat(s.count()).isEqualTo(0);
+            assertThat(s).isEmpty();
         }
 
         @Test
-        void emptyStreamWithOf() {
+        void emptyWithOf() {
             final Stream<String> s = Stream.of();
-            assertThat(s.count()).isEqualTo(0);
+            assertThat(s).isEmpty();
         }
 
         @Test
-        void streamFromIntegers() {
+        void ofInteger() {
             final Stream<Integer> s = Stream.of(1, 2, 3);
-            assertThat(s.toArray(Integer[]::new)).isEqualTo(new Integer[]{1, 2, 3});
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamFromCharacters() {
+        void ofCharacter() {
             final Stream<Character> s = Stream.of('a', 'b', 'c');
-            assertThat(s.toArray(Character[]::new)).isEqualTo(new Character[]{'a', 'b', 'c'});
+            assertThat(s).containsExactly('a', 'b', 'c');
         }
 
         @Test
-        void streamFromStrings() {
+        void ofString() {
             final Stream<String> s = Stream.of("a", "b", "c");
-            assertThat(s.toArray(String[]::new)).isEqualTo(new String[]{"a", "b", "c"});
+            assertThat(s).containsExactly("a", "b", "c");
         }
 
         @Test
-        void streamFromIntPrimitives() {
+        void ofChars() {
             final IntStream s = "test".chars();
-            final IntStream s2 = "test".codePoints();
-            assertThat(s.toArray()).isEqualTo(new int[]{116, 101, 115, 116});
-            assertThat(s2.toArray()).isEqualTo(new int[]{116, 101, 115, 116});
+            assertThat(s).containsExactly(116, 101, 115, 116);
         }
 
         @Test
-        void streamFromPattern() {
+        void ofCodePoints() {
+            final IntStream s = "test".chars();
+            assertThat(s).containsExactly(116, 101, 115, 116);
+        }
+
+        @Test
+        void ofPattern() {
             final Pattern p = Pattern.compile(",");
             final Stream<String> s = p.splitAsStream("a,b,c");
-            assertThat(s.toArray(String[]::new)).isEqualTo(new String[]{"a", "b", "c"});
+            assertThat(s).containsExactly("a", "b", "c");
         }
 
         @Test
-        void streamFromArray() {
-            final Stream<Integer> s = Stream.of(1, 2, 3);
-            assertThat(s.toArray(Integer[]::new)).isEqualTo(new Integer[]{1, 2, 3});
+        void ofArray() {
+            final Integer[] a = {1, 2, 3};
+            final Stream<Integer> s = Stream.of(a);
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamFromArrayOfIntPrimitives() {
+        void ofIntArray() {
             IntStream s = Arrays.stream(new int[]{1, 2, 3});
-            assertThat(s.toArray()).isEqualTo(new Integer[]{1, 2, 3});
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamFromArrayOfLongPrimitives() {
+        void ofLongArray() {
             final LongStream s = Arrays.stream(new long[]{1, 2, 3});
-            assertThat(s.toArray()).isEqualTo(new Long[]{1L, 2L, 3L});
+            assertThat(s).containsExactly(1L, 2L, 3L);
         }
 
         @Test
-        void streamFromArrayOfDoublePrimitives() {
+        void ofDoubleArray() {
             final DoubleStream s = Arrays.stream(new double[]{1, 2, 3});
-            assertThat(s.toArray()).isEqualTo(new Double[]{1.0, 2.0, 3.0});
+            assertThat(s).containsExactly(1.0, 2.0, 3.0);
         }
 
         @Test
-        void streamFromCollections() {
-            final List<Integer> l = Arrays.asList(1, 2, 3);
-            final Stream<Integer> s = l.stream(); // any type of collection possible
-            assertThat(s.toArray()).isEqualTo(new Integer[]{1, 2, 3});
+        void ofCollection() {
+            final List<Integer> l = List.of(1, 2, 3); // any type of collection possible
+            final Stream<Integer> s = l.stream();
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
-        void parallelStreamFromCollections() {
-            final Set<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3));
-            final Stream<Integer> ps = s.parallelStream(); // any type of collection possible
-            assertThat(ps.toArray()).isEqualTo(new Integer[]{1, 2, 3});
+        void ofCollectionParallel() {
+            final Set<Integer> _s = Set.of(1, 2, 3);
+            final Stream<Integer> s = _s.parallelStream();
+            assertThat(s).containsExactlyInAnyOrder(1, 2, 3);
         }
 
         @Test
-        void streamFromFile() throws IOException, URISyntaxException {
-            URI uri = getClass().getClassLoader().getResource("samples.txt").toURI();
+        void ofFile() throws IOException, URISyntaxException {
+            final URI uri = getClass().getClassLoader().getResource("samples.txt").toURI();
             try (final Stream<String> lines = Files.lines(Paths.get(uri))) {
-                assertThat(lines.toArray()).isEqualTo(new String[]{"one", "two", "three"});
+                assertThat(lines).containsExactly("one", "two", "three");
             }
         }
 
         @Test
         void streamBuilder() {
-            Stream<String> s = Stream.<String>builder()
-                    .add("one")
-                    .add("two")
-                    .add("three")
-                    .build();
-            assertThat(s.toArray()).isEqualTo(new String[]{"one", "two", "three"});
+            final Stream<String> s = Stream.<String>builder().add("one").add("two").add("three").build();
+            assertThat(s).containsExactly("one", "two", "three");
         }
 
         @Test
-        void streamGenerate() {
-            var s = Stream.generate(new Random()::nextInt).limit(6); // generates an infinite unordered stream
-            s.forEach(System.out::println);
+        void generate() {
+            final var s = Stream.generate(new Random()::nextInt).limit(6); // generates an infinite unordered stream
         }
 
         @Test
-        void streamIterate() {
-            var s = Stream.iterate(3, n -> n + 1).limit(6); // generates an infinite ordered stream
-            assertThat(s.toArray()).isEqualTo(new int[]{3, 4, 5, 6, 7, 8});
+        void iterate() {
+            final var s = Stream.iterate(3, n -> n + 1).limit(6); // generates an infinite ordered stream
+            assertThat(s).containsExactly(3, 4, 5, 6, 7, 8);
         }
     }
 
     @Nested
-    @DisplayName("stream odf primitives")
-    class StreamOfPrimitives {
+    class StreamPrimitiveTest {
 
         @Test
         void intStreamOf() {
-            var s = IntStream.of(1, 2, 3);
-            assertThat(s.toArray()).isEqualTo(new int[]{1, 2, 3});
+            final var s = IntStream.of(1, 2, 3);
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
         void range() {
-            var s = IntStream.range(1, 4);
-            assertThat(s.toArray()).isEqualTo(new int[]{1, 2, 3});
+            final var s = IntStream.range(1, 4);
+            assertThat(s).containsExactly(1, 2, 3);
         }
 
         @Test
         void rangeClosed() {
-            var s = IntStream.rangeClosed(1, 4);
-            assertThat(s.toArray()).isEqualTo(new int[]{1, 2, 3, 4});
+            final var s = IntStream.rangeClosed(1, 4);
+            assertThat(s).containsExactly(1, 2, 3, 4);
         }
     }
 
     @Nested
-    @DisplayName("stream conversions")
-    class StreamConversion {
-        @Test
-        void streamToList() {
-            final Stream<Integer> s = Stream.of(1, 2, 3);
-            final List<Integer> l = s.collect(Collectors.toList());
-            assertThat(l).isEqualTo(List.of(1, 2, 3));
-        }
+    class StreamConversionTest {
 
         @Test
-        void streamToCollection() {
+        void toCollection() {
             final Stream<Integer> s = Stream.of(1, 2, 3);
             final Collection<Integer> c = s.collect(Collectors.toCollection(ArrayList::new));
-            assertThat(c).isEqualTo(List.of(1, 2, 3));
+            assertThat(c).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamToListWithForEach() {
+        void toList() {
+            final Stream<Integer> s = Stream.of(1, 2, 3);
+            final List<Integer> l = s.collect(Collectors.toList());
+            assertThat(l).containsExactly(1, 2, 3);
+        }
+
+        @Test
+        void toListForEach() {
             final Stream<Integer> s = Stream.of(1, 2, 3);
             final List<Integer> l = new ArrayList<>();
             s.forEach(l::add);
-            assertThat(l).isEqualTo(List.of(1, 2, 3));
+            assertThat(l).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamToMap() {
-            final Stream<String[]> s = Stream.of(new String[][]{{"1", "one"}, {"2", "two"}});
-            final Map<String, String> m = s.collect(Collectors.toMap(e -> e[0], e -> e[1]));
-            assertThat(m).isEqualTo(new HashMap<>() {{
-                put("1", "one");
-                put("2", "two");
-            }});
+        void toArray() {
+            final Integer[] a = Stream.of(1, 2, 3).toArray(Integer[]::new);
+            assertThat(a).containsExactly(1, 2, 3);
         }
 
         @Test
-        void streamToArray() {
-            Integer[] a = Stream.of(1, 2, 3).toArray(Integer[]::new);
-            assertThat(a).isEqualTo(new Integer[]{1, 2, 3});
-        }
-
-        @Test
-        void streamToArrayOfIntPrimitives() {
+        void mapToInt() {
             final int[] a = Stream.of(1, 2, 3).mapToInt(i -> i).toArray();
-            assertThat(a).isEqualTo(new Integer[]{1, 2, 3});
+            assertThat(a).containsExactly(1, 2, 3);
         }
 
         @Test
-        void reduceStreamToString() {
+        void mapToLong() {
+            final long[] a = Stream.of(1, 2, 3).mapToLong(l -> l).toArray();
+            assertThat(a).isEqualTo(new long[]{1, 2, 3});
+        }
+
+        @Test
+        void mapToDouble() {
+            final double[] a = Stream.of(1, 2, 3).mapToDouble(d -> d).toArray();
+            assertThat(a).containsExactly(1, 2, 3);
+        }
+
+        @Test
+        void toMap() {
+            final Stream<String[]> s = Stream.of(new String[][]{{"1", "one"}, {"2", "two"}, {"3", "three"}});
+            final Map<String, String> m = s.collect(Collectors.toMap(e -> e[0], e -> e[1]));
+            assertThat(m).isEqualTo(Map.of("1", "one", "2", "two", "3", "three"));
+        }
+
+        @Test
+        void reduceToString() {
             final String s = Stream.of(1, 2, 3).map(String::valueOf).reduce((a, b) -> a + ", " + b).orElseGet(String::new);
             assertThat(s).isEqualTo("1, 2, 3");
         }
     }
 
     @Nested
-    @DisplayName("stream operations")
-    class StreamOperation {
+    class StreamOperationTest {
 
         @Test
         void findFirst() {
             Optional<Integer> o = Stream.of(1, 2, 3).findFirst();
             o.ifPresentOrElse(
-                    v -> assertThat(v).isEqualTo(1),
+                    i -> assertThat(i).isEqualTo(1),
                     () -> fail("optional is empty"));
         }
 
@@ -232,7 +239,7 @@ class StreamTest {
         void findAny() {
             Optional<Integer> o = Stream.of(1, 2, 3).findAny();
             o.ifPresentOrElse(
-                    v -> assertThat(List.of(v)).containsAnyOf(1, 2, 3),
+                    i -> assertThat(List.of(i)).containsAnyOf(1, 2, 3),
                     () -> fail("optional is empty"));
         }
 
@@ -242,61 +249,71 @@ class StreamTest {
 
         @Test
         void filter() {
-            int[] arr = Stream.of(1, 2, 3).filter(i -> i % 2 == 0).mapToInt(Integer::intValue).toArray();
-            assertThat(arr).isEqualTo(new int[]{2});
+            int[] a = Stream.of(1, 2, 3).filter(i -> i % 2 == 0).mapToInt(Integer::intValue).toArray();
+            assertThat(a).containsExactly(2);
         }
 
         @Test
         void map() {
-            int[] arr = Stream.of("one", "two", "three").map(String::length).mapToInt(Integer::intValue).toArray();
-            assertThat(arr).isEqualTo(new int[]{3, 3, 5});
+            int[] a = Stream.of("one", "two", "three").map(String::length).mapToInt(Integer::intValue).toArray();
+            assertThat(a).containsExactly(3, 3, 5);
         }
 
         @Test
         void sorted() {
-            final var sortedList = Stream.of(2, 3, 1).sorted().collect(Collectors.toList());
-            assertThat(sortedList).isEqualTo(Arrays.asList(1, 2, 3));
+            final var l = Stream.of(2, 3, 1).sorted().collect(Collectors.toList());
+            assertThat(l).containsExactly(1, 2, 3);
         }
 
         @Test
         void sortedWithComparator() {
-            final var sortedList = Stream.of(2, 3, 1).sorted(Comparator.comparing(Integer::intValue)).collect(Collectors.toList());
-            assertThat(sortedList).isEqualTo(Arrays.asList(1, 2, 3));
+            final List<Integer> l = Stream.of(2, 3, 1)
+                    .sorted(Comparator.comparing(Integer::intValue))
+                    .collect(Collectors.toList());
+            assertThat(l).containsExactly(1, 2, 3);
         }
 
         @Test
         void reduce() {
-            BiFunction<Integer, Integer, Integer> summe = (a, b) -> a + b;
-            Integer sum = Stream.of(1, 2, 3).reduce(1 /*seed*/, (a, b) -> a + b);
+            final BinaryOperator<Integer> summing = (a, b) -> a + b;
+            final int sum = Stream.of(1, 2, 3).reduce(1 /*seed*/, summing);
             assertThat(sum).isEqualTo(7);
         }
 
         @Test
-        void reduceWithSum() {
-            Integer sum = Stream.of(1, 2, 3).reduce(1 /*seed*/, Integer::sum);
+        void reduceWithIntegerSum() {
+            final int sum = Stream.of(1, 2, 3).reduce(1 /*seed*/, Integer::sum);
             assertThat(sum).isEqualTo(7);
         }
 
         @Test
-        void sumOnIntStream() {
-            Integer sum = Stream.of(1, 2, 3).mapToInt(Integer::intValue).sum();
+        void sum() {
+            final int sum = Stream.of(1, 2, 3).mapToInt(Integer::intValue).sum();
             assertThat(sum).isEqualTo(6);
         }
 
         @Test
         void count() {
-            var count = Stream.of(1, 2, 3).count();
+            final long count = Stream.of(1, 2, 3).count();
             assertThat(count).isEqualTo(3);
         }
 
         @Test
         void average() {
-            OptionalDouble avg = Stream.of(1, 2, 3).mapToInt(Integer::intValue).average();
-            avg.ifPresentOrElse(v -> assertThat(v).isEqualTo(2.0), () -> fail("empty optional double"));
+            final OptionalDouble o = Stream.of(1, 2, 3).mapToDouble(d -> d).average();
+            o.ifPresentOrElse(
+                    avg -> assertThat(avg).isEqualTo(2.0),
+                    () -> fail("empty optional double"));
         }
 
         @Test
         void collect() {
+        }
+
+        @Test
+        void limit() {
+            Stream<Integer> s = Stream.of(1, 2, 3).limit(1);
+            assertThat(s).containsOnly(1);
         }
 
         @Test
@@ -327,10 +344,11 @@ class StreamTest {
                     new Pair<>(1, "B"),
                     new Pair<>(2, "C"),
                     new Pair<>(3, "D"));
-            final Map<Integer, List<Pair<Integer, String>>> groupedBy = pairs.stream().collect(Collectors.groupingBy(Pair::getValue0));
-            assertThat(groupedBy.get(1)).isEqualTo(List.of(pairs.get(0), pairs.get(1)));
-            assertThat(groupedBy.get(2)).isEqualTo(List.of(pairs.get(2)));
-            assertThat(groupedBy.get(3)).isEqualTo(List.of(pairs.get(3)));
+            final Map<Integer, List<Pair<Integer, String>>> groupedBy = pairs.stream()
+                    .collect(Collectors.groupingBy(Pair::getValue0));
+            assertThat(groupedBy.get(1)).containsExactly(pairs.get(0), pairs.get(1));
+            assertThat(groupedBy.get(2)).containsExactly(pairs.get(2));
+            assertThat(groupedBy.get(3)).containsExactly(pairs.get(3));
         }
 
         @Test
@@ -340,26 +358,26 @@ class StreamTest {
                     .collect(Collectors.groupingBy(i -> number.getAndIncrement() / 2))
                     .values();
             final List<List<Integer>> partitionedList = new ArrayList<>(partitionedCollection);
-            assertThat(partitionedList.get(0)).isEqualTo(List.of(1, 2));
-            assertThat(partitionedList.get(1)).isEqualTo(List.of(3, 4));
-            assertThat(partitionedList.get(2)).isEqualTo(List.of(5));
+            assertThat(partitionedList.get(0)).containsExactly(1, 2);
+            assertThat(partitionedList.get(1)).containsExactly(3, 4);
+            assertThat(partitionedList.get(2)).containsExactly(5);
         }
     }
 
     @Nested
-    @DisplayName("Java 9 stream add-ons")
-    class Java9Stream {
+    @DisplayName("Java 9 add-ons")
+    class Java9StreamTest {
 
         @Test
         void takeWhile() {
             final Integer[] array = Stream.of(0, 2, 5, 6, 8).takeWhile(n -> n % 2 == 0).toArray(Integer[]::new);
-            assertThat(array).isEqualTo(new Integer[]{0, 2});
+            assertThat(array).containsExactly(0, 2);
         }
 
         @Test
         void dropWhile() {
             final Integer[] array = Stream.of(0, 2, 5, 6, 8).dropWhile(n -> n % 2 == 0).toArray(Integer[]::new);
-            assertThat(array).isEqualTo(new Integer[]{5, 6, 8});
+            assertThat(array).containsExactly(5, 6, 8);
         }
     }
 }
